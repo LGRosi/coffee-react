4d9310c5ec815fcd6ff29d47169cfdbdb2bec352
Lucas <lrosi1996@gmail.com>
2021-07-27 16:34:45 -0300

    Componente Banner de la Home

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React from "react";
+import TazaGranoMobile from "../images/TazaGrano_mobile.png";
+import TazaGranoTablet from "../images/TazaGrano_tablet.png";
+import TazaGrano from "../images/TazaGrano.png";
 
 class Banner extends React.Component {
-    render () {
-        return (
-            <section>
-                <p>Banner</p>
-            </section>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div className="row d-flex justify-content-center">
+          <div className="col-10 col-sm-8 col-md-6 col-lg-4 mb-2">
+            <p className="my-2 h1 text-center border-top border-bottom">
+              Coffee shop brand
+            </p>
+            <h2 className="text-center h3">Familiar company</h2>
+          </div>
+          <div className="col-12 d-flex justify-content-center">
+            <figure>
+              <picture>
+                <source media="(max-width: 599px)" srcSet={TazaGranoMobile} />
+                <source media="(max-width: 800px)" srcSet={TazaGranoTablet} />
+                <img src={TazaGrano} width="950" className="img-fluid" alt="Taza de café" />
+              </picture>
+            </figure>
+            
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Banner;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import tazaDeCafe from '../images/TazaDeCafe.png';
-// import Banner from "../components/Banner";
+import Banner from "../components/Banner";
 
 class Home extends React.Component {
   render() {
@@ -8,15 +7,7 @@ class Home extends React.Component {
       <React.Fragment>
         <section className="container mt-5">
           <h1 className="d-none">Cafetería</h1>
-          <div className="row d-flex justify-content-center">
-            <div className="col-4 mb-5">
-              <p className="my-2 h1 text-center border-top border-bottom">Coffee shop brand</p>
-              <h2 className="text-center h3">Familiar company</h2>
-            </div>
-            <div className="col-12">
-              <img src={tazaDeCafe} width="400" alt="Taza de café" />
-            </div>
-          </div>
+          <Banner />
         </section>
       </React.Fragment>
     );
