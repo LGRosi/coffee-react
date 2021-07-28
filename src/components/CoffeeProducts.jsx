a806437760697e2fcfc83df62e65db1e2d46e113
Lucas <lrosi1996@gmail.com>
2021-07-28 16:42:36 -0300

    Actualización del componente CoffeeProducts

--- src/components/CoffeeProducts.jsx
+++ src/components/CoffeeProducts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import './styles/CoffeeProducts.css';
+import "./styles/CoffeeProducts.css";
+import carrito from "../images/Carrito.svg";
 import GranosCafe from "../images/GranosCafe.svg";
 import cafe1 from "../images/cafe1.png";
 import cafe2 from "../images/cafe2.png";
@@ -21,40 +22,104 @@ class CoffeeProducts extends React.Component {
               The most delicious grain in the entire region
             </h2>
 
-            <div className="col-12 mb-4">
+            <div className="col-12 mt-4 mb-4">
               <div className="row">
                 <div className="col-4">
                   <div className="card border-0" width="18rem">
-                    <img src={cafe1} className="card-img-top SizeCoffee4 m-auto" alt="..." />
-                    <div className="card-body">
-                      <p className="card-text">
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
+                    <img
+                      src={cafe1}
+                      className="card-img-top SizeCoffee4 m-auto mt-2"
+                      alt="..."
+                    />
+                    <div className="card-body m-auto w-75 border-bottom">
+                      <h3 className="h5 ColorH3">Guatemala Antigua</h3>
+                      <p className="card-text mt-3">
+                        MEDIUM ROAST | Coffee beans Mildly spicy cocoa aroma
                       </p>
+                      <div className="row">
+                        <div className="col-6">
+                          <div>
+                            <p className="mt-1 fw-bold">US$2.88</p>
+                          </div>
+                        </div>
+                        <div className="col-6 d-flex justify-content-center">
+                          <div>
+                            <img
+                              src={carrito}
+                              width="18"
+                              alt="Carrito de compras"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <button type="button" className="btn btn-light w-100 border border-secondary">Buy</button>
                     </div>
                   </div>
                 </div>
 
                 <div className="col-4">
                   <div className="card border-0" width="18rem">
-                    <img src={cafe2} className="card-img-top SizeCoffee2 m-auto" alt="..." />
-                    <div className="card-body">
-                      <p className="card-text">
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
+                    <img
+                      src={cafe2}
+                      className="card-img-top SizeCoffee2 m-auto"
+                      alt="..."
+                    />
+                    <div className="card-body m-auto w-75 border-bottom">
+                      <h3 className="h5 ColorH3">Colombia</h3>
+                      <p className="card-text mt-3">
+                        MEDIUM ROAST | Coffee beans Intense flavor and nutty
+                        nuance
                       </p>
+                      <div className="row">
+                        <div className="col-6">
+                          <div>
+                            <p className="mt-1 fw-bold">US$2.99</p>
+                          </div>
+                        </div>
+                        <div className="col-6 d-flex justify-content-center">
+                          <div>
+                            <img
+                              src={carrito}
+                              width="18"
+                              alt="Carrito de compras"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <button type="button" className="btn btn-light w-100 border border-secondary">Buy</button>
                     </div>
                   </div>
                 </div>
 
                 <div className="col-4">
                   <div className="card border-0" width="18rem">
-                    <img src={cafe3} className="card-img-top SizeCoffee m-auto" alt="..." />
-                    <div className="card-body">
-                      <p className="card-text">
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
+                    <img
+                      src={cafe3}
+                      className="card-img-top SizeCoffee m-auto"
+                      alt="..."
+                    />
+                    <div className="card-body m-auto w-75 border-bottom">
+                      <h3 className="h5 ColorH3">Kenya</h3>
+                      <p className="card-text mt-3">
+                        MEDIUM ROAST | Coffee beans Juicy, intense and complex
                       </p>
+                      <div className="row">
+                        <div className="col-6">
+                          <div>
+                            <p className="fw-bold">US$2.50</p>
+                          </div>
+                        </div>
+                        <div className="col-6 d-flex justify-content-center">
+                          <div>
+                            <img
+                              src={carrito}
+                              width="18"
+                              alt="Carrito de compras"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <button type="button" className="btn btn-light w-100 border border-secondary">Buy</button>
                     </div>
                   </div>
                 </div>
@@ -65,36 +130,99 @@ class CoffeeProducts extends React.Component {
               <div className="row">
                 <div className="col-4">
                   <div className="card border-0" width="18rem">
-                    <img src={cafe4} className="card-img-top SizeCoffee m-auto" alt="..." />
-                    <div className="card-body">
-                      <p className="card-text">
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
+                    <img
+                      src={cafe4}
+                      className="card-img-top SizeCoffee m-auto mt-4"
+                      alt="..."
+                    />
+                    <div className="card-body m-auto w-75 border-bottom">
+                      <h3 className="h5 ColorH3">Sumatra</h3>
+                      <p className="card-text mt-3">
+                        DARK ROAST | Coffee beans Earthy and herbaceous
                       </p>
+                      <div className="row">
+                        <div className="col-6">
+                          <div>
+                            <p className="mt-1 fw-bold">US$2.75</p>
+                          </div>
+                        </div>
+                        <div className="col-6 d-flex justify-content-center">
+                          <div>
+                            <img
+                              src={carrito}
+                              width="18"
+                              alt="Carrito de compras"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <button type="button" className="btn btn-light w-100 border border-secondary">Buy</button>
                     </div>
                   </div>
                 </div>
 
                 <div className="col-4">
                   <div className="card border-0" width="18rem">
-                    <img src={cafe5} className="card-img-top SizeCoffee m-auto" alt="..." />
-                    <div className="card-body">
-                      <p className="card-text">
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
+                    <img
+                      src={cafe5}
+                      className="card-img-top SizeCoffee m-auto"
+                      alt="..."
+                    />
+                    <div className="card-body m-auto w-75 border-bottom">
+                      <h3 className="h5 ColorH3">Verona</h3>
+                      <p className="card-text mt-3">
+                        DARK ROAST | Coffee grains Black cocoa, roasted sweet
                       </p>
+                      <div className="row">
+                        <div className="col-6">
+                          <div>
+                            <p className="mt-1 fw-bold">US$2.98</p>
+                          </div>
+                        </div>
+                        <div className="col-6 d-flex justify-content-center">
+                          <div>
+                            <img
+                              src={carrito}
+                              width="18"
+                              alt="Carrito de compras"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <button type="button" className="btn btn-light w-100 border border-secondary">Buy</button>
                     </div>
                   </div>
                 </div>
 
                 <div className="col-4">
                   <div className="card border-0" width="18rem">
-                    <img src={cafe6} className="card-img-top SizeCoffee3 m-auto" alt="..." />
-                    <div className="card-body">
-                      <p className="card-text">
-                        Some quick example text to build on the card title and
-                        make up the bulk of the card's content.
+                    <img
+                      src={cafe6}
+                      className="card-img-top SizeCoffee3 m-auto pb-0"
+                      alt="..."
+                    />
+                    <div className="card-body m-auto w-75 pt-0 border-bottom">
+                      <h3 className="h5 ColorH3">Espresso Roast</h3>
+                      <p className="card-text mt-3">
+                        DARK ROAST | Coffee beans Aromatic and caramelized
                       </p>
+                      <div className="row">
+                        <div className="col-6">
+                          <div>
+                            <p className="mt-1 fw-bold">US$2.60</p>
+                          </div>
+                        </div>
+                        <div className="col-6 d-flex justify-content-center">
+                          <div>
+                            <img
+                              src={carrito}
+                              width="18"
+                              alt="Carrito de compras"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <button type="button" className="btn btn-light w-100 border border-secondary">Buy</button>
                     </div>
                   </div>
                 </div>
