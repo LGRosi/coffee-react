5020ac9a2197fbffabcc3fba5a6660a8f636d4e2
Lucas <lrosi1996@gmail.com>
2021-07-30 21:33:25 -0300

    Error 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import NotFound404 from '../images/404.svg';
 
 function NotFound () {
     return (
         <React.Fragment>
-            <h1>404: Not Found</h1>
+            <section className="container">
+                <div className="row d-flex justify-content-center">
+                    <img src={NotFound404} className="w-50" alt="Not Found Error 404" />
+                </div>
+            </section>
         </React.Fragment>
     )
 }
