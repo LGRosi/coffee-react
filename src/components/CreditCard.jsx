c9d492fe8d7aefc6a57a0ca6c4bb507a783d0ac4
Lucas <lrosi1996@gmail.com>
2021-07-31 21:25:20 -0300

    Componente CreditCard

---
+++ src/components/CreditCard.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import "./styles/CreditCard.css";
+import Visa from "../images/visa.png";
+import Chip from "../images/chip-tarjeta.png";
+import NFC from "../images/nfc.png";
+
+class CreditCard extends React.Component {
+  render() {
+    const GiroTarjeta = () => {
+      const tarjeta = document.querySelector(".tarjeta");
+      tarjeta.classList.toggle("active");
+    };
+
+    return (
+      <React.Fragment>
+        <section className="container my-5">
+          <div className="row">
+            <div className="col-12 d-flex justify-content-center">
+              <div
+                className="tarjeta"
+                onClick={() => {
+                  GiroTarjeta();
+                }}
+              >
+                <div className="delantera">
+                  <div className="logo-marca">
+                    <img src={NFC} className="nfc" alt="Logo de NFC" />
+                    <img src={Visa} alt="Logo de visa" />
+                  </div>
+                  <img src={Chip} className="chip" alt="Chip de la tarjeta" />
+                  <div className="datos">
+                    <div className="grupo">
+                      <p className="label">Card number</p>
+                      <p className="numero">#### #### #### ####</p>
+                    </div>
+                    <div className="flexbox">
+                      <div className="grupo">
+                        <p className="label">Cardholder Name</p>
+                        <p className="nombre">LUCAS ROSI</p>
+                      </div>
+
+                      <div className="grupo">
+                        <p className="label">Expiration</p>
+                        <p className="expiracion">
+                          <span className="mes">MM</span> /{" "}
+                          <span className="year">YY</span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="trasera h-100">
+                  <div className="barra-magnetica"></div>
+                  <div className="datos w-100">
+                    <div className="grupo">
+                      <p className="label">Firm</p>
+                      <div className="firma">
+                        <p></p>
+                      </div>
+                    </div>
+                    <div className="grupo">
+                      <p className="label">CCV</p>
+                      <p className="ccv"></p>
+                    </div>
+                  </div>
+                  <p className="leyenda">
+                    This card is issued by a global company in accordance with
+                    the Visa license. If you have any questions you can contact
+                    global coffee.
+                  </p>
+                  <a href="/" className="link-banco">
+                    www.coffee.com
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </React.Fragment>
+    );
+  }
+}
+
+export default CreditCard;
+
+// <section className="contenedor tarjeta" id="tarjeta">
+//         <div className="delantera">
+//           <div className="logo-marca" id="logo-marca">
+//             <img src={Visa} alt="Tarjeta Visa" />
+//           </div>
+//           <img src={Chip} className="chip" alt="Chip de la tarjeta" />
+//           <div className="datos">
+//             <div className="grupo" id="numero">
+//               <p className="label">Número Tarjeta</p>
+//               <p className="numero">#### #### #### ####</p>
+//             </div>
+//             <div className="flexbox">
+//               <div className="grupo" id="nombre">
+//                 <p className="label">Nombre Tarjeta</p>
+//                 <p className="nombre">Jhon Doe</p>
+//               </div>
+
+//               <div className="grupo" id="expiracion">
+//                 <p className="label">Expiracion</p>
+//                 <p className="expiracion">
+//                   <span className="mes">MM</span> / <span className="year">AA</span>
+//                 </p>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+
+//         <div className="trasera">
+//           <div className="barra-magnetica"></div>
+//           <div className="datos">
+//             <div className="grupo" id="firma">
+//               <p className="label">Firma</p>
+//               <div className="firma">
+//                 <p></p>
+//               </div>
+//             </div>
+//             <div className="grupo" id="ccv">
+//               <p className="label">CCV</p>
+//               <p className="ccv"></p>
+//             </div>
+//           </div>
+//           <p className="leyenda">
+//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus
+//             exercitationem, voluptates illo.
+//           </p>
+//           <a href="/" className="link-banco">
+//             www.tubanco.com
+//           </a>
+//         </div>
+//       </section>
--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import CreditCard from "../components/CreditCard";
 
 class Carrito extends React.Component {
-    render () {
-        return 'Carrito';
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <section className="container">
+          <h1 className="text-center">Credit Card</h1>
+          <div className="row">
+            <div className="col-6">
+              <CreditCard />
+            </div>
+          </div>
+        </section>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Carrito;
