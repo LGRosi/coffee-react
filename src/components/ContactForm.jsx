f4b16e2859b0b07725decb05b758d68693ad8d20
Lucas <lrosi1996@gmail.com>
2021-08-29 19:38:15 -0300

    Actualización de ContactForm

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,42 +1,59 @@
 import React from "react";
-import './styles/ContactForm.css';
+import "./styles/ContactForm.css";
 
-class ContactForm extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <form action="/" className="formulario-tarjeta">
-          <div className="grupo">
-            <input
-              className="inputNumero"
-              type="text"
-              placeholder="First name"
-              name="cardNumber"
-              autoComplete="off"
-            />
+function ContactForm () {
+  return (
+    <React.Fragment>
+        <section className="container mt-3">
+          <div className="row">
+            <div className="col-12">
+              <h2 className="h3">Contact</h2>
+              <p>Leave us all your doubts</p>
+            </div>
+            <div className="col-12">
+              <form action="/" className="formulario-tarjeta">
+                <div className="grupo">
+                  <input
+                    className="inputNumero"
+                    type="text"
+                    placeholder="First name"
+                    name="cardNumber"
+                    autoComplete="off"
+                  />
+                </div>
+                <div>
+                  <input
+                    className="inputNumero"
+                    type="text"
+                    placeholder="Last name"
+                    name="cardNumber"
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="grupo">
+                  <input
+                    className="inputNumero"
+                    type="text"
+                    placeholder="Email"
+                    name="cardNumber"
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="grupo">
+                  <textarea
+                    className="inputNumero"
+                    placeholder="Leave us your comment"
+                  ></textarea>
+                </div>
+                <button type="button" className="btn-enviar">
+                  Send
+                </button>
+              </form>
+            </div>
           </div>
-          <div className="grupo">
-            <input
-              className="inputNumero"
-              type="text"
-              placeholder="Last name"
-              name="cardNumber"
-              autoComplete="off"
-            />
-          </div>
-          <div className="grupo">
-            <input
-              className="inputNumero"
-              type="text"
-              placeholder="Cellphone"
-              name="cardNumber"
-              autoComplete="off"
-            />
-          </div>
-        </form>
+        </section>
       </React.Fragment>
-    );
-  }
+  );
 }
 
 export default ContactForm;
